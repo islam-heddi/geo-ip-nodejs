848de69d14b187758e0556e7dae35be1a470af6d
islam heddi <islamheddi19@gmail.com>
2024-07-26 14:32:39 +0100

    added geoip country

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const { lookup } = require('geoip-lite')
+const { lookup } = require('geoip-country')
 
 const PORT = process.env.PORT || 5000
 
