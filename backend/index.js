2c52009438bed5547ff3e3de40e5e65cea407d06
islam heddi <islamheddi19@gmail.com>
2024-07-28 16:15:28 +0100

    changed the null message

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.get('/country', (req,res) => {
     const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress
     const this_ip = ip.split(',')[0]
     console.log(this_ip)
-    res.send('Adress ip : ' + this_ip + '<br>the lookup :  ' + JSON.stringify(lookup(this_ip)))
+    res.send('Adress ip : ' + this_ip + '<br>the lookup :  ' + (JSON.stringify(lookup(this_ip)?JSON.stringify(lookup(this_ip)): "None country or Localhost")))
 })
 
 app.listen(PORT, () => {
