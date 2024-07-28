4057c0b8a33254c07654923fb70a7a597839a288
islam heddi <islamheddi19@gmail.com>
2024-07-28 15:36:42 +0100

    split(,)

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.get('/', (req,res) => {
 
 app.get('/country', (req,res) => {
     const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress
-    const this_ip = ip.split(' ')[0]
+    const this_ip = ip.split(',')[0]
     console.log(this_ip)
     res.send('Adress ip : ' + this_ip + '<br>the lookup :  ' + JSON.stringify(lookup(this_ip)))
 })
